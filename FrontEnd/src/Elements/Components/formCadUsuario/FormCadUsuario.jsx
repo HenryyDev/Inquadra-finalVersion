02a2c46eeb0e4e544e40cbfaec9393b4cfc3f1af
henry4i20 <henrimito1305@gmail.com>
2024-11-22 11:15:26 -0300

    cadastro de usuario funcionando!

--- FrontEnd/src/Elements/Components/formCadUsuario/FormCadUsuario.jsx
+++ FrontEnd/src/Elements/Components/formCadUsuario/FormCadUsuario.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
 import "../../Css/CadUsuario.css";
 import logo from "../../../assets/logo.png";
-import ver from "../../../assets/ver.png"
-import ocultar from "../../../assets/ocultar.png"
+import ver from "../../../assets/ver.png";
+import ocultar from "../../../assets/ocultar.png";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const CadUsuario = () => {
   const [values, setValues] = useState({
     nome: "",
-    cpf: "",
     email: "",
     senha: "",
     confirmarSenha: "",
-    termos: false
+    termos: false,
   });
 
   const [errors, setErrors] = useState({});
@@ -21,24 +22,35 @@ const CadUsuario = () => {
   const validateForm = () => {
     const newErrors = {};
     if (!values.nome) newErrors.nome = "Nome é obrigatório!";
-    if (!values.cpf) newErrors.cpf = "Cpf é obrigatório!";
+
     if (!values.email) newErrors.email = "Email é obrigatório!";
     if (!values.senha) newErrors.senha = "Senha é obrigatório!";
-    if (values.senha != values.confirmarSenha) newErrors.confirmarSenha = "Senha diferente ou nula!"
+    if (values.senha != values.confirmarSenha)
+      newErrors.confirmarSenha = "Senha diferente ou nula!";
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (validateForm()) {
-      console.log(values);
-      alert("Usuário criado!");
+    if (!validateForm()) {
+      toast.error("corrija os erros!")
+      return
     }
+    axios.post("http://localhost:3000/usuario",values)
+    .then((resposta)=>{
+      console.log("resposta da req:",resposta)
+      toast.success("Conta criado com sucesso!");
+    })
+    .catch((erro)=>{
+      console.log("erro na req:",erro);
+      toast.error("Erro ao criar conta")
+    })
   };
 
-
   return (
+    <>
+    <ToastContainer />
     <div className="wrap-user">
       <div className="cad-user">
         <form onSubmit={handleSubmit}>
@@ -47,7 +59,9 @@ const CadUsuario = () => {
           </Link>
           <h2 className="h2-txt">Crie sua conta. É grátis!</h2>
           <div className="mb-3">
-            <label htmlFor="nome" className="form-label">Nome Completo</label>
+            <label htmlFor="nome" className="form-label">
+              Nome Completo
+            </label>
             {errors.nome && (
               <div className="alert alert-danger" role="alert">
                 <span className="error-text">{errors.nome}</span>
@@ -60,20 +74,11 @@ const CadUsuario = () => {
               value={values.nome}
               onChange={(e) => setValues({ ...values, nome: e.target.value })}
             />
-            <label htmlFor="cpf" className="form-label">CPF</label>
-            {errors.cpf && (
-              <div className="alert alert-danger" role="alert">
-                <span className="error-text">{errors.cpf}</span>
-              </div>
-            )}
-            <input
-              type="text"
-              className="form-control nome"
-              id="cpf"
-              value={values.cpf}
-              onChange={(e) => setValues({ ...values, cpf: e.target.value })}
-            />
-            <label htmlFor="email" className="form-label">Email</label>
+            
+
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
             {errors.email && (
               <div className="alert alert-danger" role="alert">
                 <span className="error-text">{errors.email}</span>
@@ -91,7 +96,9 @@ const CadUsuario = () => {
             </div>
           </div>
           <div className="mb-3" style={{ position: "relative" }}>
-            <label htmlFor="senha" className="form-label" >Senha</label>
+            <label htmlFor="senha" className="form-label">
+              Senha
+            </label>
             {errors.senha && (
               <div className="alert alert-danger" role="alert">
                 <span className="error-text">{errors.senha}</span>
@@ -104,7 +111,9 @@ const CadUsuario = () => {
                 className="form-control"
                 id="senha"
                 value={values.senha}
-                onChange={(e) => setValues({ ...values, senha: e.target.value })}
+                onChange={(e) =>
+                  setValues({ ...values, senha: e.target.value })
+                }
               />
               <img
                 src={viewSenha ? ver : ocultar}
@@ -114,21 +123,21 @@ const CadUsuario = () => {
                 style={{
                   position: "absolute",
                   right: "10px",
-                  cursor: "pointer"
+                  cursor: "pointer",
                 }}
                 onClick={() => setViewSenha((estado) => !estado)}
               />
             </div>
           </div>
           <div className="mb-3" style={{ position: "relative" }}>
-
-            <label htmlFor="senha" className="form-label" >Confirmar senha</label>
+            <label htmlFor="senha" className="form-label">
+              Confirmar senha
+            </label>
             {errors.confirmarSenha && (
               <div className="alert alert-danger" role="alert">
                 <span className="error-text">{errors.confirmarSenha}</span>
               </div>
             )}
-
 
             <div className="senha">
               <input
@@ -137,9 +146,10 @@ const CadUsuario = () => {
                 className="form-control"
                 id="senha"
                 value={values.confirmarSenha}
-                onChange={(e) => setValues({ ...values, confirmarSenha: e.target.value })}
+                onChange={(e) =>
+                  setValues({ ...values, confirmarSenha: e.target.value })
+                }
               />
-
             </div>
           </div>
           <div className="mb-3 form-check">
@@ -149,16 +159,22 @@ const CadUsuario = () => {
               className="form-check-input"
               id="exampleCheck1"
               checked={values.termos}
-              onChange={(e) => setValues({ ...values, termos: e.target.checked })}
+              onChange={(e) =>
+                setValues({ ...values, termos: e.target.checked })
+              }
             />
             <label className="form-check-label" htmlFor="exampleCheck1">
-              Eu aceito os <a href="../Termos/index.html">termos e condições.</a>
+              Eu aceito os{" "}
+              <a href="../Termos/index.html">termos e condições.</a>
             </label>
           </div>
-          <button type="submit" className="btn btn-primary">Criar conta</button>
+          <button type="submit" className="btn btn-primary">
+            Criar conta
+          </button>
         </form>
       </div>
     </div>
+    </>
   );
 };
 
