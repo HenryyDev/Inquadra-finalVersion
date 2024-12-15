77833037d1ca4679dea72dd0cc738409fd4d5fd1
henry4i20 <henrimito1305@gmail.com>
2024-12-15 20:50:00 -0300

    teste

--- Back/db.js
+++ Back/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Carregar as variáveis de ambiente
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'InQuadra',
+  host: process.env.DB_HOST,        // Use a variável de ambiente
+  user: process.env.DB_USER,        // Use a variável de ambiente
+  password: process.env.DB_PASSWORD, // Use a variável de ambiente
+  database: process.env.DB_NAME,     // Use a variável de ambiente
+  port: process.env.DB_PORT || 3306, // Defina a porta do MySQL, com fallback para 3306
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
