3375a616aae337fee5d5dd05e2fe5382715fc41f
henry4i20 <henrimito1305@gmail.com>
2024-12-15 21:21:42 -0300

    teste

--- Back/db.js
+++ Back/db.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2');
-require('dotenv').config(); // Carregar as variáveis de ambiente
+require('dotenv').config();  // Carrega as variáveis de ambiente do .env
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,        // Use a variável de ambiente
-  user: process.env.DB_USER,        // Use a variável de ambiente
-  password: process.env.DB_PASSWORD, // Use a variável de ambiente
-  database: process.env.DB_NAME,     // Use a variável de ambiente
-  port: process.env.DB_PORT || 3306, // Defina a porta do MySQL, com fallback para 3306
+  host: process.env.MYSQL_HOST,       // Host do banco de dados (por exemplo, "localhost" ou o host do Railway)
+  user: process.env.MYSQL_USER,       // Nome de usuário do banco de dados
+  password: process.env.MYSQL_PASSWORD, // Senha do banco de dados
+  database: process.env.MYSQL_DATABASE, // Nome do banco de dados
+  port: process.env.MYSQL_PORT,       // Porta do banco de dados (geralmente 3306 para MySQL)
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
